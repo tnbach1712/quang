1f0524a91de9ddb813e8a030178c7d58b89db7aa
vinrex <trannguyenbach1992@gmail.com>
2020-04-28 15:12:30 +0700

    update

--- app.js
+++ app.js
@@ -36,5 +36,11 @@ app.use(function(err, req, res, next) {
   });
 });
 
-module.exports = app;
-
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
